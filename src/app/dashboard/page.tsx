0519e4608c0a07d497a00dbc478d66488726035d
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-06-11 18:07:48 -0300

    feat: implement session management in authentication and dashboard pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,23 @@
-export default function DashboardPage() {
-  return <div>Dashboard</div>;
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+import { auth } from '@/lib/auth';
+
+import SignOutButton from './components/sign-out-button';
+
+export default async function DashboardPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    redirect('/authentication');
+  }
+  return (
+    <>
+      <div>Dashboard</div>
+      <p>Welcome {session?.user.name}!</p>
+      <p>E-mail: {session?.user.email}</p>
+      <SignOutButton />
+    </>
+  );
 }
